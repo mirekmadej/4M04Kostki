af6cd0463ef3d54882f1fc9409408f2c201b74e7
Mirosław Madej <M.Madej@zsp3zamosc.pl>
2024-09-24 12:36:44 +0200

    Dodaj pliki projektów.

---
+++ 4M04Kostki/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace _4M04Kostki
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ 4M04Kostki/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿namespace _4M04Kostki
+{
+    public partial class MainPage : ContentPage
+    {
+
+        int suma = 0;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRzutClicked(object sender, EventArgs e)
+        {
+            suma = 0;
+            int rzut;
+            Random r = new Random();
+
+            Image[] kostki = new Image[] { k00, k01, k10, k11, k20, k21 };
+
+            /*
+            rzut = r.Next(1, 7);
+            k00.Source = "kosc" + rzut.ToString() + ".png";
+            suma += rzut;
+            */
+
+            
+            foreach(var k in kostki)
+            {
+                rzut = r.Next(1, 7);
+                k.Source = "kosc" + rzut.ToString() + ".png";
+                suma += rzut;
+            }
+
+
+            lblSuma.Text = $"Suma: {suma}";
+        }
+
+
+    }
+
+}
